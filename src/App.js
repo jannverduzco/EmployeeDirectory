1b245bcfaeb1a9427ed1a07f4dde73c041c6c619
Janneth Verduzco <janneth_verduzco@live.com>
2021-03-14 00:00:10 -0600

    restructured code in App.js and components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
+import API from "../utils/API";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import Container from "./components/Container";
 import EmployeeSearch from "./components/EmployeeSearch";
 import EmployeeDisplay from "./components/EmployeeDisplay";
-import API from "./utils/API";
 
-function App() {
-  const [employeeState, setEmployeeState] = useState([]);
+class App extends Component {
+  state = {
+    search: "",
+    results: [],
+  };
 
-  useEffect(() => {
-    API.getEmployees().then(res => {setEmployeeState(res.data.results)})
-  }, []);
-  return (
-    <div>
-      <Wrapper>
-        <Container>
-          <EmployeeSearch />
-          <EmployeeDisplay />
-        </Container>
-      </Wrapper>
-      <Footer />
-    </div>
-  );
+  componentDidMount() {
+    this.filterEmployees("employees");
+  }
+
+  filterEmployees = (query) => {
+    API.search(query)
+      .then((res) => this.setState({ results: res.data.data }))
+      .catch((err) => console.log(err));
+  };
+
+  // return (
+  //   <div>
+  //     <Wrapper>
+  //       <Container>
+  //         <EmployeeSearch />
+  //         <EmployeeDisplay />
+  //       </Container>
+  //     </Wrapper>
+  //     <Footer />
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ src/components/EmployeeSearch/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import Container from "../components/Container";
+import SearchForm from "../components/SearchForm";
+import SearchResults from "../components/SearchResults";
+import Alert from "../components/Alert";
+
