d71236019fd9a6601adee24792291f28ffb5074b
Janneth Verduzco <janneth_verduzco@live.com>
2021-03-16 22:34:53 -0500

    generated API rendered data on browser

--- src/components/EmployeeSearch/index.js
+++ src/components/EmployeeSearch/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
-import EmployeeDisplayResults from "../EmployeeDisplayResults";
 import SearchForm from "../SearchForm";
 import "./style.css";
 
@@ -8,33 +7,46 @@ class EmployeeSearch extends Component {
   state = {
     search: "",
     results: [],
+    filteredResults: [],
   };
 
   componentDidMount() {
+    this.fetchEmployees();
+  }
+
+  fetchEmployees = () => {
     API.getEmployees()
       .then((res) =>
         this.setState({
-          EmployeeDisplayResults: res.data.results,
+          results: res.data.results,
           filteredResults: res.data.results,
         })
       )
       .catch((err) => console.log(err));
-  }
+  };
 
-  filterEmployees = (query) => {
-    API.getEmployees(query)
-      .then((res) => this.setState({ results: res.data.data }))
-      .catch((err) => console.log(err));
+  renderResults = () => {
+    return this.state.filteredResults.map((results) => {
+      return (
+        <div className="row">
+          <img src={results.picture.thumbnail} alt="image"></img>
+          {/* <div className="col-sm">Name {results.name.first}{results.name.last}</div>
+          <div className="col-sm">Email {results.email}</div>
+          <div className="col-sm">Phone {results.cell}</div>
+          <div className="col-sm">Date of Birth {results.dob}</div>
+          <div className="col-sm">State {results.location.state}</div> */}
+        </div>
+      );
+    });
   };
 
   render() {
     return (
       <div>
-    <SearchForm />
-    <EmployeeDisplayResults />
-    </div>
+        <SearchForm />
+        {this.renderResults()}
+      </div>
     );
   }
 }
-
 export default EmployeeSearch;
